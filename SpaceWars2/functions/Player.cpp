74fc378c648a24ef4db51909f4c9d8404fb79af3
subaru2003 <subaru2003.main@gmail.com>
2018-10-08 13:59:47 +0900

    斜めに動いたときの挙動･枠への食い込みを修正

--- SpaceWars2/functions/Player.cpp
+++ SpaceWars2/functions/Player.cpp
@@ -14,7 +14,22 @@ void Player::Control(){
 	Rect tmpZoon;// = Rect(0, 0, 0, 0);
 	tmpX = this->posX;
 	tmpY = this->posY;
+
 	if(this->isLeft){
+		tmpZoon = Rect(0, 0, Config::Width / 2, Config::Height);
+		if(Input::KeyD.pressed && tmpZoon.contains(Circle(this->posX + SPEED, this->posY, 40)))			this->posX += SPEED;
+		if(Input::KeyA.pressed && tmpZoon.contains(Circle(this->posX - SPEED, this->posY, 40)))			this->posX -= SPEED;
+		if(Input::KeyW.pressed && tmpZoon.contains(Circle(this->posX, this->posY - SPEED, 40)))			this->posY -= SPEED;
+		if(Input::KeyS.pressed && tmpZoon.contains(Circle(this->posX, this->posY + SPEED, 40)))			this->posY += SPEED;
+	}else{
+		tmpZoon = Rect(Config::Width/2, 0, Config::Width / 2, Config::Height);
+		if(Input::KeySemicolon.pressed && tmpZoon.contains(Circle(this->posX + SPEED, this->posY, 40)))	this->posX += SPEED;
+		if(Input::KeyK.pressed && tmpZoon.contains(Circle(this->posX - SPEED, this->posY, 40)))			this->posX -= SPEED;
+		if(Input::KeyO.pressed && tmpZoon.contains(Circle(this->posX, this->posY - SPEED, 40)))			this->posY -= SPEED;
+		if(Input::KeyL.pressed && tmpZoon.contains(Circle(this->posX, this->posY + SPEED, 40)))			this->posY += SPEED;
+	}
+	this->ship = Circle(this->posX, this->posY, 40);
+	/*if(this->isLeft){
 		if(Input::KeyD.pressed)			this->posX += SPEED;
 		else if(Input::KeyA.pressed)	this->posX -= SPEED;
 		if(Input::KeyW.pressed)			this->posY -= SPEED;
@@ -31,5 +46,5 @@ void Player::Control(){
 	if(tmpZoon.contains(this->ship) == false){
 		this->posX = tmpX;
 		this->posY = tmpY;
-	}
+	}*/
 }
