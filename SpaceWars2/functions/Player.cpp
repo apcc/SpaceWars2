5b0aaf06df1bc52382a980a03653833be51db49f
subaru2003 <subaru2003.main@gmail.com>
2018-10-08 16:07:16 +0900

    コメントアウト消し忘れてた

--- SpaceWars2/functions/Player.cpp
+++ SpaceWars2/functions/Player.cpp
@@ -11,7 +11,7 @@ void Player::Init(int32 _x, int32 _y, bool _isLeft){
 
 void Player::Control(){
 	int32 tmpX = 0, tmpY = 0;
-	Rect tmpZoon;// = Rect(0, 0, 0, 0);
+	Rect tmpZoon;
 	tmpX = this->posX;
 	tmpY = this->posY;
 
@@ -29,22 +29,4 @@ void Player::Control(){
 		if(Input::KeyL.pressed && tmpZoon.contains(Circle(this->posX, this->posY + SPEED, 40)))			this->posY += SPEED;
 	}
 	this->ship = Circle(this->posX, this->posY, 40);
-	/*if(this->isLeft){
-		if(Input::KeyD.pressed)			this->posX += SPEED;
-		else if(Input::KeyA.pressed)	this->posX -= SPEED;
-		if(Input::KeyW.pressed)			this->posY -= SPEED;
-		else if(Input::KeyS.pressed)	this->posY += SPEED;
-		tmpZoon = Rect(0, 0, Config::Width / 2, Config::Height);
-	}else{
-		if(Input::KeySemicolon.pressed)	this->posX += SPEED;
-		else if(Input::KeyK.pressed)	this->posX -= SPEED;
-		if(Input::KeyO.pressed)			this->posY -= SPEED;
-		else if(Input::KeyL.pressed)	this->posY += SPEED;
-		tmpZoon = Rect(Config::Width/2, 0, Config::Width / 2, Config::Height);
-	}
-	this->ship = Circle(this->posX, this->posY, 40);
-	if(tmpZoon.contains(this->ship) == false){
-		this->posX = tmpX;
-		this->posY = tmpY;
-	}*/
 }
