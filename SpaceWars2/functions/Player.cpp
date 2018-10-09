7121fd4e5405253f7a34a04a411773f0256dde81
subaru2003 <subaru2003.main@gmail.com>
2018-10-09 12:36:17 +0900

    いろいろ修正

--- SpaceWars2/functions/Player.cpp
+++ SpaceWars2/functions/Player.cpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "Player.hpp"
 
-#define SPEED 3
+#define PLAYER_SPEED 10
 
 void Player::Init(int32 _x, int32 _y, bool _isLeft){	
 	posX = _x;
@@ -10,23 +10,20 @@ void Player::Init(int32 _x, int32 _y, bool _isLeft){
 }
 
 void Player::Control(){
-	int32 tmpX = 0, tmpY = 0;
 	Rect tmpZoon;
-	tmpX = this->posX;
-	tmpY = this->posY;
 
-	if(this->isLeft){
+	if(isLeft){
 		tmpZoon = Rect(0, 0, Config::Width / 2, Config::Height);
-		if(Input::KeyD.pressed && tmpZoon.contains(Circle(this->posX + SPEED, this->posY, 40)))			this->posX += SPEED;
-		if(Input::KeyA.pressed && tmpZoon.contains(Circle(this->posX - SPEED, this->posY, 40)))			this->posX -= SPEED;
-		if(Input::KeyW.pressed && tmpZoon.contains(Circle(this->posX, this->posY - SPEED, 40)))			this->posY -= SPEED;
-		if(Input::KeyS.pressed && tmpZoon.contains(Circle(this->posX, this->posY + SPEED, 40)))			this->posY += SPEED;
+		if(Input::KeyD.pressed && tmpZoon.contains(Circle(posX + PLAYER_SPEED, posY, 40)))			posX += PLAYER_SPEED;
+		if(Input::KeyA.pressed && tmpZoon.contains(Circle(posX - PLAYER_SPEED, posY, 40)))			posX -= PLAYER_SPEED;
+		if(Input::KeyW.pressed && tmpZoon.contains(Circle(posX, posY - PLAYER_SPEED, 40)))			posY -= PLAYER_SPEED;
+		if(Input::KeyS.pressed && tmpZoon.contains(Circle(posX, posY + PLAYER_SPEED, 40)))			posY += PLAYER_SPEED;
 	}else{
 		tmpZoon = Rect(Config::Width/2, 0, Config::Width / 2, Config::Height);
-		if(Input::KeySemicolon.pressed && tmpZoon.contains(Circle(this->posX + SPEED, this->posY, 40)))	this->posX += SPEED;
-		if(Input::KeyK.pressed && tmpZoon.contains(Circle(this->posX - SPEED, this->posY, 40)))			this->posX -= SPEED;
-		if(Input::KeyO.pressed && tmpZoon.contains(Circle(this->posX, this->posY - SPEED, 40)))			this->posY -= SPEED;
-		if(Input::KeyL.pressed && tmpZoon.contains(Circle(this->posX, this->posY + SPEED, 40)))			this->posY += SPEED;
+		if(Input::KeySemicolon.pressed && tmpZoon.contains(Circle(posX + PLAYER_SPEED, posY, 40)))	posX += PLAYER_SPEED;
+		if(Input::KeyK.pressed && tmpZoon.contains(Circle(posX - PLAYER_SPEED, posY, 40)))			posX -= PLAYER_SPEED;
+		if(Input::KeyO.pressed && tmpZoon.contains(Circle(posX, posY - PLAYER_SPEED, 40)))			posY -= PLAYER_SPEED;
+		if(Input::KeyL.pressed && tmpZoon.contains(Circle(posX, posY + PLAYER_SPEED, 40)))			posY += PLAYER_SPEED;
 	}
-	this->ship = Circle(this->posX, this->posY, 40);
+	ship = Circle(posX, posY, 40);
 }
