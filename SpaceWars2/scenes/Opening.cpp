5a7797265999291555d9644a0675e8b32558efd9
subaru2003 <subaru2003.main@gmail.com>
2018-10-09 15:11:48 +0900

    Init関数の引数にConfigを使うように

--- SpaceWars2/scenes/Opening.cpp
+++ SpaceWars2/scenes/Opening.cpp
@@ -6,8 +6,8 @@ void Opening::init(){
 	this->CicaR32 = Font(32, L"Cica"); // :ac:
 	m_data->count++;
 	Println(m_data->count);
-	m_data->LPlayer.Init(40,   360, true);
-	m_data->RPlayer.Init(1240, 360, false);
+	m_data->LPlayer.Init(40,   Config::Height/2, true);  //円の半径
+	m_data->RPlayer.Init(1240, Config::Height/2, false); //Width-円の半径
 }
 
 void Opening::update(){
