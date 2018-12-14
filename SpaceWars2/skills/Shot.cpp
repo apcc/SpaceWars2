60e4a7aa7de47e8fd679ebba90c4405eda836978
subaru2003 <subaru2003.main@gmail.com>
2018-12-14 13:08:10 +0900

    getShape()を使うように修正

--- SpaceWars2/skills/Shot.cpp
+++ SpaceWars2/skills/Shot.cpp
@@ -5,11 +5,11 @@ bool Shot::update() {
 }
 
 void Shot::draw(){
-	this->getShape().draw(Color(L"#ffff00"));
+	getShape().draw(Color(L"#ffff00"));
 }
 
 bool Shot::isInvisible(){
-	return !Circle(pos, 8).intersects(Rect(0, 0, Config::Width + 1, Config::Height + 1));
+	return !getShape().intersects(Rect(0, 0, Config::Width + 1, Config::Height + 1));
 }
 
 bool Shot::intersects(Circle circle) {
