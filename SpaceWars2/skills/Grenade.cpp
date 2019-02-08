0a04e7ea026ac82245934b2b4f4ed4fdcdba2719
subaru2003 <subaru2003.main@gmail.com>
2019-02-08 13:01:48 +0900

    Grenade.cppのコーディングスタイルを修正

--- SpaceWars2/skills/Grenade.cpp
+++ SpaceWars2/skills/Grenade.cpp
@@ -1,15 +1,15 @@
 #include "./Grenade.hpp"
 
 bool Grenade::update() {
-	if(fuse==0) return true;
-	if(fuse<=EXPLODE_TIMING)vel.set(0,0);
-	fuse--;
+	if(fuse == 0) return true;
+	if(fuse <= EXPLODE_TIMING) vel.set(0,0);
+	--fuse;
 	return Bullet::update();
 }
 
 void Grenade::draw(){
 	if(fuse<=EXPLODE_TIMING){
-		getShape().draw(ColorF(L"#ff8800").setAlpha(((double)fuse)/EXPLODE_TIMING));
+		getShape().draw(ColorF(L"#ff8800").setAlpha(((double)fuse) / EXPLODE_TIMING));
 	}else{
 		getShape().draw(Color(L"#ff0000"));
 	}
@@ -21,15 +21,15 @@ bool Grenade::isInvisible(){
 
 int Grenade::getDamage(Circle circle){
 	if(circle.intersects(this->getShape())){
-		if(fuse>EXPLODE_TIMING)
+		if(fuse > EXPLODE_TIMING)
 			this->explode();
-		if(fuse==EXPLODE_TIMING)
-			return (EXPLODE_RADIUS-pos.distanceFrom(circle.center))/EXPLODE_RADIUS*10;
+		if(fuse == EXPLODE_TIMING)
+			return (int)((EXPLODE_RADIUS - pos.distanceFrom(circle.center)) / EXPLODE_RADIUS * 10);
 		return 0;
 	}
 	return 0;
 }
 
 void Grenade::explode(){
-	fuse=EXPLODE_TIMING;
+	fuse = EXPLODE_TIMING;
 }
