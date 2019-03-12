a4c3909c45a6eccba95654e39121d3356a7b2e64
subaru2003 <subaru2003.main@gmail.com>
2019-03-12 19:05:19 +0900

    Rename file: config.hpp->Config.hpp

---
+++ SpaceWars2/Config.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <Siv3D.hpp>
+
+namespace Config {
+
+	// タイトル
+	const String Title		= L"SPACE WARS 2";
+
+	// サイズ
+	const int32  Width		= 1280;
+	const int32  Height		= 720;
+
+	// 背景色
+	const String Background	= L"#ffffff";
+}
