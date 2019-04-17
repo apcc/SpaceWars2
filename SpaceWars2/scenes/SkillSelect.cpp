c074a3ddcdd817d704f8efe5adc7ea67f11d8d39
subaru2003 <subaru2003.main@gmail.com>
2019-04-17 16:40:32 +0900

    前後のskillを描画

--- SpaceWars2/scenes/SkillSelect.cpp
+++ SpaceWars2/scenes/SkillSelect.cpp
@@ -1,5 +1,7 @@
 #include "SkillSelect.hpp"
 
+#define V80 { 80, 80 }
+
 void SkillSelect::init() {
 	int i = 0;
 	for (int j = -1; j < 6; j++) {
@@ -31,12 +33,25 @@ void SkillSelect::draw() const {
 	FontAsset(L"CicaR32")(Data::RPlayer.whatMainSkill).draw(1240, 40);
 	FontAsset(L"CicaR32")(Data::RPlayer.whatSubSkill).draw(1240, 80);
 	FontAsset(L"CicaR32")(Data::RPlayer.whatSpecialSkill).draw(1240, 120);
+	
+	TextureAsset(L"main" + Format(Data::LPlayer.whatMainSkill - 1)).resize(V80).drawAt(130, 450);
+	TextureAsset(L"sub" + Format(Data::LPlayer.whatSubSkill - 1)).resize(V80).drawAt(320, 450);
+	TextureAsset(L"special" + Format(Data::LPlayer.whatSpecialSkill - 1)).resize(V80).drawAt(510, 450);
+	TextureAsset(L"main" + Format(Data::RPlayer.whatMainSkill - 1)).resize(V80).drawAt(770, 450);
+	TextureAsset(L"sub" + Format(Data::RPlayer.whatSubSkill - 1)).resize(V80).drawAt(960, 450);
+	TextureAsset(L"special" + Format(Data::RPlayer.whatSpecialSkill - 1)).resize(V80).drawAt(1150, 450);
 
 	TextureAsset(L"main" + Format(Data::LPlayer.whatMainSkill)).drawAt(130, 550);
 	TextureAsset(L"sub" + Format(Data::LPlayer.whatSubSkill)).drawAt(320, 550);
 	TextureAsset(L"special" + Format(Data::LPlayer.whatSpecialSkill)).drawAt(510, 550);
-
 	TextureAsset(L"main" + Format(Data::RPlayer.whatMainSkill)).drawAt(770, 550);
 	TextureAsset(L"sub" + Format(Data::RPlayer.whatSubSkill)).drawAt(960, 550);
 	TextureAsset(L"special" + Format(Data::RPlayer.whatSpecialSkill)).drawAt(1150, 550);
+
+	TextureAsset(L"main" + Format(Data::LPlayer.whatMainSkill + 1)).resize(V80).drawAt(130, 650);
+	TextureAsset(L"sub" + Format(Data::LPlayer.whatSubSkill + 1)).resize(V80).drawAt(320, 650);
+	TextureAsset(L"special" + Format(Data::LPlayer.whatSpecialSkill + 1)).resize(V80).drawAt(510, 650);
+	TextureAsset(L"main" + Format(Data::RPlayer.whatMainSkill + 1)).resize(V80).drawAt(770, 650);
+	TextureAsset(L"sub" + Format(Data::RPlayer.whatSubSkill + 1)).resize(V80).drawAt(960, 650);
+	TextureAsset(L"special" + Format(Data::RPlayer.whatSpecialSkill + 1)).resize(V80).drawAt(1150, 650);
 }
