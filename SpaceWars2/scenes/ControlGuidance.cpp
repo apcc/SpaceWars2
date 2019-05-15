6dc5f2d674592f32e029f2ce52a0576eae8a9f4c
subaru2003 <subaru2003.main@gmail.com>
2019-05-15 23:40:35 +0900

    drawPointLine()の問題を修正して重なりを改善

--- SpaceWars2/scenes/ControlGuidance.cpp
+++ SpaceWars2/scenes/ControlGuidance.cpp
@@ -19,32 +19,35 @@ void ControlGuidance::draw() const {
 
 	drawPointLine({ 540, cY + 75 }, {  265, cY + 75 });
 	drawPointLine({ 445, cY - 40 }, {  265, cY - 40 });
-	drawPointLine({ 830, cY + 15 }, { 1015, cY + 15 }, Color(L"#afa"));
+	drawPointLine({ 830, cY + 15 }, { 1015, cY + 75 }, Color(L"#afa"));
 	drawPointLine({ 880, cY - 35 }, { 1015, cY - 35 }, Color(L"#faa"));
 
 	drawPointLine({435, cY - 190}, { 265, cY - 225});
-	drawPointLine({450, cY - 195}, { 265, cY - 260});
+	drawPointLine({450, cY - 195}, { 265, cY - 300});
 	drawPointLine({845, cY - 190}, {1015, cY - 225});
-	drawPointLine({830, cY - 195}, {1015, cY - 260});
+	drawPointLine({830, cY - 195}, {1015, cY - 300});
 
 	TextureAsset(L"stick_64").drawAt(225, cY + 75);
 	TextureAsset(L"cross_64").drawAt(225, cY - 40);
-	TextureAsset(L"buttonA_64").drawAt(1055, cY + 15);
+	TextureAsset(L"buttonA_64").drawAt(1055, cY + 75);
 	TextureAsset(L"buttonB_64").drawAt(1055, cY - 35);
 
 	TextureAsset(L"buttonLB_64").drawAt( 225, cY - 225);
-	TextureAsset(L"buttonLT_64").drawAt( 225, cY - 260);
+	TextureAsset(L"buttonLT_64").drawAt( 225, cY - 300);
 	TextureAsset(L"buttonRB_64").drawAt(1055, cY - 225);
-	TextureAsset(L"buttonRT_64").drawAt(1055, cY - 260);
+	TextureAsset(L"buttonRT_64").drawAt(1055, cY - 300);
 }
 
 
 void ControlGuidance::drawPointLine(Vec2 _root, Vec2 _pos, Color _color) {
 	int isLeft = 0; // -1:left 0:center 1:right
+	int xSlope = 0;
 	if (_pos.x > _root.x) isLeft = -1;
 	if (_pos.x < _root.x) isLeft = 1;
+	if (_pos.y > _root.y) xSlope = -1;
+	if (_pos.y < _root.y) xSlope = 1;
 	double move = _pos.y - _root.y;
 
 	Circle(_root, 6).draw(_color);
-	LineString({ _root, _root.movedBy(move * isLeft, move), _pos }).draw(5, _color);
+	LineString({ _root, _root.movedBy(move * isLeft * xSlope, move), _pos }).draw(5, _color);
 }
