88eaf0b44401373c858612eb15b34cdb189bc12f
yamahi1610 <yamahi1610@gmail.com>
2019-06-13 19:45:49 +0900

    効果時間と倍率を強化

--- SpaceWars2/skills/InversionRecovery.hpp
+++ SpaceWars2/skills/InversionRecovery.hpp
@@ -12,7 +12,7 @@ private:
 	int RecoverAmount = 1;
 	bool inRecovery;
 
-	const int TRICKING_TIME = 90;
+	const int TRICKING_TIME = 120;
 
 public:
 	InversionRecovery(Vec2 _pos, bool _isLeft) : Bullet(_pos, _isLeft) {
