e3efd00316c2e60adbaae059860b6c8d5321f6cd
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2019-06-18 15:51:09 +0900

    Main,Sub,Special対応ボタン数を増やした

--- SpaceWars2/functions/XInput.cpp
+++ SpaceWars2/functions/XInput.cpp
@@ -1,6 +1,7 @@
 #include "XInput.hpp"
 
 #define BORDER 0.1
+#define FOUR_BUTTONS(num) XInput(num).buttonA|XInput(num).buttonB|XInput(num).buttonX|XInput(num).buttonY
 
 using asc::XInput;
 
@@ -49,8 +50,8 @@ void GamePad::SetAxis() {
 
 void GamePad::SetButton() {
 	// MainSkill
-	input.addButton(L"L_MainSkill", s3d::Input::KeyQ | XInput(0).buttonB);
-	input.addButton(L"R_MainSkill", s3d::Input::KeyI | s3d::Input::KeySlash | XInput(1).buttonB);
+	input.addButton(L"L_MainSkill", s3d::Input::KeyQ | FOUR_BUTTONS(0));
+	input.addButton(L"R_MainSkill", s3d::Input::KeyI | s3d::Input::KeySlash | FOUR_BUTTONS(1));
 
 	// SubSkill
 	input.addButton(L"L_SubSkill", s3d::Input::KeyE | XInput(0).buttonLB);
@@ -76,8 +77,8 @@ void GamePad::SetButton() {
 	input.addButton(L"KeyEnter", s3d::Input::KeyEnter | XInput(0).buttonA | XInput(1).buttonA);
 	input.addButton(L"L_KeyBack", s3d::Input::KeyLControl | s3d::Input::KeyBackspace | s3d::Input::KeyEnd | XInput(0).buttonB);
 	input.addButton(L"R_KeyBack", s3d::Input::KeyRControl | s3d::Input::KeyBackspace | s3d::Input::KeyEnd | XInput(1).buttonB);
-	input.addButton(L"L_KeySelect", s3d::Input::KeyLShift | XInput(0).buttonA);
-	input.addButton(L"R_KeySelect", s3d::Input::KeyRShift | XInput(1).buttonA);
+	input.addButton(L"L_KeySelect", s3d::Input::KeyLShift | FOUR_BUTTONS(0));
+	input.addButton(L"R_KeySelect", s3d::Input::KeyRShift | FOUR_BUTTONS(1));
 }
 
 
@@ -126,6 +127,12 @@ bool GamePad::Key(bool _isLeft, const String& _name) {
 			|| XInput((int)!_isLeft). leftTrigger > 0.5 
 			|| XInput((int)!_isLeft).rightTrigger > 0.5 
 			|| s3d::Input::KeyBackspace.pressed;
+
+	if (_name == L"SubSkill")
+		return input.button(lr + L"_SubSkill").pressed || XInput((int)!_isLeft).leftTrigger > 0.8;
+
+	if (_name == L"SpecialSkill")
+		return input.button(lr + L"_SpecialSkill").pressed || XInput((int)!_isLeft).rightTrigger > 0.8;
 
 	return input.button(lr + L"_" + _name).pressed;
 }
