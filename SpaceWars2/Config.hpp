baae8cb9b89ca897b930d9afacaf48731d760efb
subaru2003 <subaru2003.main@gmail.com>
2019-06-19 19:23:46 +0900

    Release 1.4.1_d

--- SpaceWars2/Config.hpp
+++ SpaceWars2/Config.hpp
@@ -4,8 +4,8 @@
 
 namespace Config {
 	// version
-	const String VERSION	   = L"1.4_d";
-	const int	 VER_NUM	   = 19061918;
+	const String VERSION	   = L"1.4.1_d";
+	const int	 VER_NUM	   = 19061919;
 
 	// タイトル
 	const String TITLE		   = L"SpaceWars 2";
